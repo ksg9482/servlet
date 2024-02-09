f083c9a5312235b26bfffc8a135a5a2c0e5f098a
ksg9482 <ksg930523@gmail.com>
2024-02-09 23:36:16 +0900

    servlet

--- sevlet/src/main/java/hello/sevlet/ServletInitializer.java
+++ sevlet/src/main/java/hello/sevlet/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SevletApplication.class);
+		return application.sources(ServletApplication.class);
 	}
 
 }
---
+++ sevlet/src/main/java/hello/sevlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.sevlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String userName = request.getParameter("username");
+        System.out.println("userName = " + userName);
+
+        response.setContentType("text/plain"); //응답할 데이터 타입
+        response.setCharacterEncoding("utf-8"); //데이터 인코딩
+        response.getWriter().write("hello " + userName); //실제로 데이터 입력
+    }
+}
