a96c1d2c9268f0ec85923cf62aae654a82161f4e
ksg9482 <ksg930523@gmail.com>
2024-02-14 21:56:23 +0900

    servlet

---
+++ sevlet/src/main/java/hello/sevlet/domain/member/MemberRepository.java
@@ -0,0 +1,40 @@
+package hello.sevlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    //싱글톤은 생성자 막아야 함
+    private  MemberRepository() {}
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    //어레이리스트 새로 만들어서 반환하는 이유: 어레이리스트를 조작해도 스토어에 있는 값은 건들지 않기 위함
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+}
---
+++ sevlet/src/test/java/hello/sevlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package hello.sevlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    //싱글톤이라 new안됨
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //then
+        Member savedMember = memberRepository.save(member);
+
+        //when
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //then
+        List<Member> result = memberRepository.findAll();
+
+        //when
+        assertThat(result.size()).isEqualTo(2);
+        //객체안에 두 인스턴스가 있는가 여부
+        assertThat(result).contains(member1, member2);
+    }
+}
